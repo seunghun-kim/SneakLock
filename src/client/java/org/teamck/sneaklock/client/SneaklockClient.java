e75fa5806e0256c9885629bb19b1ccc6e1121877
Seunghun Kim <seunghunkim@icloud.com>
2025-04-29 10:55:36 +0900

    Initial Commit

---
+++ src/client/java/org/teamck/sneaklock/client/SneaklockClient.java
@@ -0,0 +1,10 @@
+package org.teamck.sneaklock.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SneaklockClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/org/teamck/sneaklock/Sneaklock.java
@@ -0,0 +1,10 @@
+package org.teamck.sneaklock;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Sneaklock implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
